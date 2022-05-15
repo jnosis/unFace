dfb0d55a0ec2117cff9b211070d18d8ddda37484
jnosis <jnosis1546@gmail.com>
2022-05-15 17:25:26 +0900

    Add About styles

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,10 +1,23 @@
 import React, { forwardRef } from 'react';
-import './about.module.css';
+import styles from './about.module.css';
 
 const About = forwardRef<HTMLElement>((_, scrollRef) => {
   return (
-    <section ref={scrollRef} id='home'>
-      <h1>About</h1>
+    <section ref={scrollRef} className={styles.container}>
+      <article className={styles.card}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>About</h1>
+          <div className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </article>
     </section>
   );
 });
