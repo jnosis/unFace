02c39ab2bf0579eb5f49ca87f39dab0f359894cf
jnosis <jnosis1546@gmail.com>
2023-01-13 16:25:44 +0900

    Init fresh project

---
+++ main.ts
@@ -0,0 +1,13 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { start } from '$fresh/server.ts';
+import manifest from '~/fresh.gen.ts';
+
+import freshwind from 'freshwind/plugin.ts';
+import twindConfig, { configUrl } from '~/twind.config.ts';
+
+await start(manifest, { plugins: [freshwind(twindConfig, configUrl)] });
--- src/util/validator.ts
+++
@@ -1,8 +0,0 @@
-export function validateTitle(title: string): boolean {
-  return title.length > 0 && title[0] !== ' ';
-}
-
-export function validateRepo(repoURL: string): boolean {
-  const repoRegExps = [/^(https?\:\/\/)?github.com\/[\w]+\/[\w]+/];
-  return repoRegExps.every((exp) => exp.test(repoURL));
-}
