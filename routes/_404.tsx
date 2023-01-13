70095549f622b4eb6a8d30b263cc8b7fa54c34bf
jnosis <jnosis1546@gmail.com>
2023-01-13 16:48:25 +0900

    Add _404 route

---
+++ routes/_404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <section class='flex flex-col justify-center items-center w-full h-full mt-16 pt-8'>
+      <h1 class='text-3xl font-bold'>Not Found</h1>
+      <a href='/'>Go To Home</a>
+    </section>
+  );
+}
