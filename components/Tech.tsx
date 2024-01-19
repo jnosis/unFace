03c4cd2d1a8f89de3c8b83fff640a85b1d60e918
jnosis <jnosis1546@gmail.com>
2024-01-19 16:36:04 +0900

    fix: Apply text color correctly to Tech component

--- components/Tech.tsx
+++ components/Tech.tsx
@@ -7,12 +7,12 @@ type TechProps = {
 export default function Tech({ name, selected, clickable }: TechProps) {
   return (
     <li
-      class={`h-6 px-4 rounded-lg font-bold text-xs leading-6 text-on-surface-variant dark:text-on-surface-variant-dark ${
+      class={`h-6 px-4 rounded-lg font-bold text-xs leading-6 ${
         selected
           ? clickable
-            ? 'bg-secondary-container dark:bg-secondary-container-dark'
-            : 'bg-tertiary-container dark:bg-tertiary-container-dark'
-          : 'outline outline-1 outline-outline dark:outline-outline-dark'
+            ? 'bg-secondary-container dark:bg-secondary-container-dark text-on-secondary-container dark:text-on-secondary-container-dark'
+            : 'bg-tertiary-container dark:bg-tertiary-container-dark text-on-tertiary-container dark:text-on-tertiary-container-dark'
+          : 'outline outline-1 outline-outline dark:outline-outline-dark text-on-surface-variant dark:text-on-surface-variant-dark'
       }${clickable ? ' cursor-pointer' : ''}`}
       data-tech={name}
     >
